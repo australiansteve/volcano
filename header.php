fa5ca7309a7f34f58fd4582fdd1c4f31dce3426a
australiansteve <steve@australiansteve.com>
2015-12-16 04:30:19 -0600

    Reshuffle the menus again, make the header a bit smaller in general

--- header.php
+++ header.php
@@ -25,7 +25,7 @@
 
 		<div class="row"><!-- .row start -->
 
-			<div class="small-12 medium-4 columns"><!-- .columns start -->
+			<div class="small-12 medium-2 columns"><!-- .columns start -->
 
 				<div class="site-branding">
 
@@ -63,21 +63,38 @@
 
 			</div><!-- .columns end -->
 
-			<div class="small-12 medium-8 columns show-for-medium"><!-- .columns start -->
+			<div class="small-12 medium-10 columns"><!-- .columns start -->
 
-				<div class="row"><!-- .row start -->
+				<div class="row">
+				
+					<div class="small-12 columns"><!-- .columns start -->
 
-					<div class="medium-12 columns"><!-- .columns start -->
+						<div class="title-bar show-for-small-only" data-responsive-toggle="primary-menu" data-hide-for="medium">
+								<button class="menu-icon" type="button" data-toggle=""></button>
+								<div class="title-bar-title">Menu</div>
+						</div>
 
-						<div class="top-bar secondary-navigation" data-topbar role="navigation">
-						
+						<div id="primary-menu" class="top-bar primary-navigation" data-topbar>
+							
 							<div class="top-bar-right">
 
 								<!-- Right Nav Section -->
-								<ul class="medium-horizontal menu">
+								<ul class="vertical medium-horizontal menu">
 
+									<?php
+									 	$args = array (
+										 	'theme_location' 	=> 'primary',
+										 	'container' 		=> '',
+										 	'menu_class' 		=> '',
+										 	'menu_id' 			=> '',
+										 	'items_wrap' 		=> '%3$s'
+									 	);
+										wp_nav_menu( $args );
+									?>
+								</ul>
+
+								<ul class="vertical medium-horizontal menu show-for-small-only">
 									<?php 
-
 									 	$args = array (
 										 	'theme_location' 	=> 'secondary',
 										 	'container' 		=> '',
@@ -85,19 +102,20 @@
 										 	'menu_id' 			=> '',
 										 	'items_wrap' 		=> '%3$s'
 									 	);
-										wp_nav_menu( $args ); 
+										wp_nav_menu( $args );
 									?>
 
 								</ul>
 
 							</div>
+
 						</div>
 
 					</div><!-- .columns end -->
 
-				</div><!-- .row.menus -->
+				</div><!-- .row -->
 
-				<div class="row"><!-- .row start -->
+				<div class="row show-for-medium"><!-- .row start -->
 
 					<div class="small-12 columns"><!-- .columns start -->
 
@@ -129,58 +147,43 @@
 
 				</div><!-- .row.menus -->
 
+				<div class="row show-for-medium"><!-- .row start -->
+
+					<div class="medium-12 columns "><!-- .columns start -->
+
+						<div class="top-bar secondary-navigation" data-topbar role="navigation">
+						
+							<div class="top-bar-right">
+
+								<!-- Right Nav Section -->
+								<ul class="medium-horizontal menu">
+
+									<?php 
+
+									 	$args = array (
+										 	'theme_location' 	=> 'secondary',
+										 	'container' 		=> '',
+										 	'menu_class' 		=> '',
+										 	'menu_id' 			=> '',
+										 	'items_wrap' 		=> '%3$s'
+									 	);
+										wp_nav_menu( $args ); 
+									?>
+
+								</ul>
+
+							</div>
+
+						</div>
+
+					</div><!-- .columns end -->
+
+				</div><!-- .row.menus -->
+
 			</div><!-- .columns end -->
 
 		</div><!-- .row end -->
 
-		<div class="row">
-		
-			<div class="small-12 columns"><!-- .columns start -->
-
-				<div class="title-bar show-for-small-only" data-responsive-toggle="primary-menu" data-hide-for="medium">
-						<button class="menu-icon" type="button" data-toggle=""></button>
-						<div class="title-bar-title">Menu</div>
-				</div>
-
-				<div id="primary-menu" class="top-bar primary-navigation" data-topbar>
-					
-					<div class="top-bar-right">
-
-						<!-- Right Nav Section -->
-						<ul class="vertical medium-horizontal menu">
-
-							<?php
-							 	$args = array (
-								 	'theme_location' 	=> 'primary',
-								 	'container' 		=> '',
-								 	'menu_class' 		=> '',
-								 	'menu_id' 			=> '',
-								 	'items_wrap' 		=> '%3$s'
-							 	);
-								wp_nav_menu( $args );
-							?>
-						</ul>
-
-						<ul class="vertical medium-horizontal menu show-for-small-only">
-							<?php 
-							 	$args = array (
-								 	'theme_location' 	=> 'secondary',
-								 	'container' 		=> '',
-								 	'menu_class' 		=> '',
-								 	'menu_id' 			=> '',
-								 	'items_wrap' 		=> '%3$s'
-							 	);
-								wp_nav_menu( $args );
-							?>
-
-						</ul>
-
-					</section>
-				</div>
-
-			</div><!-- .columns end -->
-
-		</div>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">

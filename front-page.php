1b9a551aab1f9ad4a54ea0ead2067b833182916f
australiansteve <steve@australiansteve.com>
2016-02-09 15:09:24 -0600

    Change number of front-page widgets in gallery preview from 2 to 3

--- front-page.php
+++ front-page.php
@@ -56,7 +56,7 @@ get_header(); ?>
 
 </div><!-- .row end -->
 
-<div class="row small-up-1 medium-up-2 austeve_gallery_preview"><!-- .row start -->
+<div class="row small-up-1 medium-up-3 austeve_gallery_preview"><!-- .row start -->
 
 	<?php dynamic_sidebar( 'austeve_gallery_1' ); ?>
 

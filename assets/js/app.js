457a173f2d1618e22e2ddb6ace335d642a70b30e
australiansteve <steve@australiansteve.com>
2016-03-20 22:29:56 -0300

    Scroll to image when preview image is clicked

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,8 +3,12 @@ jQuery(document).ready(function($) {
 	$(document).foundation();
 
 	jQuery("img.change").click(function(){
-		console.log("Clicked:" + $(this).attr("data-change"));
 		
+		//console.log("Clicked:" + $(this).attr("data-change"));
 		$("#"+$(this).attr("data-change")).html($(this).clone().removeClass("change"));
+
+		//Scroll to the image (mainly for small screens)
+		var topPos = $("#"+$(this).attr("data-change")).offset();
+		window.scrollTo(0, topPos.top);
 	});
 });
